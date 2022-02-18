624e933aef889c58fbec8c09e15700d2615f9c01
davindev <davinjeong.dev@gmail.com>
2022-02-18 19:23:38 +0900

    add: 공통 Button 컴포넌트 추가

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef } from 'react';
+
+import * as S from './Button.styled';
+
+interface Props {
+  type?: 'button' | 'submit';
+  color?: 'primary' | 'secondary';
+  variant?: 'contain' | 'outline';
+  width?: number;
+  height?: number;
+  disabled?: boolean;
+  children: React.ReactNode;
+}
+
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ children, ...props }: Props, ref) => (
+    <S.Button
+      {...props}
+      ref={ref}
+    >
+      {children}
+    </S.Button>
+  ),
+);
+
+export default Button;
+
+Button.defaultProps = {
+  type: 'button',
+  color: 'primary',
+  variant: 'contain',
+  width: undefined,
+  height: undefined,
+  disabled: false,
+};
---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const ORANGE = '#fba405';
+export const ORANGE_DARKER = '#f19e05';
+export const ORANGE_WHITER = '#fff8ec';
+
+export const CYAN = '#0297ad';
+export const CYAN_DARKER = '#03899f';
+export const CYAN_WHITER = '#e6f5f7';
+
+export const GRAY = '#e9ecef';
+export const GRAY_DARKER = '#7d848b';
