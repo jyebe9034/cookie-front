f8712494d8266b5b95e8f5988a225a90768b9cf6
davindev <davinjeong.dev@gmail.com>
2022-03-10 22:31:06 +0900

    feat: 로그인 버튼 추가 및 UserInfo 컴포넌트 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from '@mui/material/Button';
+
+import UserInfo from './UserInfo';
+
 export default function Header() {
   return (
-    <div className="flex items-center px-10 py-6">
-      <Link to="/">
-        <h1 className="font-bold text-2xl">tumuto</h1>
-      </Link>
-      <ul className="flex gap-2 ml-10">
-        <li>
-          <Link to="/board" className="px-4 py-3 text-lg">게시판</Link>
-        </li>
-      </ul>
+    <div className="flex items-center justify-between px-10 py-6">
+      <div className="flex items-center">
+        <Link to="/">
+          <h1 className="font-bold text-2xl">
+            tumuto
+          </h1>
+        </Link>
+        <ul className="flex gap-2 ml-10">
+          <li>
+            <Link
+              to="/board"
+              className="px-4 py-3 text-lg"
+            >
+              게시판
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <UserInfo />
+      {/* <Button
+        type="button"
+        variant="contained"
+        color="primary"
+        disableElevation
+        sx={{ color: '#fff' }}
+      >
+        로그인
+      </Button> */}
     </div>
   );
 }
---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Avatar from '@mui/material/Avatar';
+
+export default function UserInfo() {
+  return (
+    <button
+      type="button"
+    >
+      <Avatar sx={{ width: 30, height: 30 }} />
+    </button>
+  );
+}
