8c3be81ddeeadc6d1723df94f3a186c32064ca6f
davindev <davinjeong.dev@gmail.com>
2022-03-10 20:52:20 +0900

    fix: Header 컴포넌트에서 서비스명 수정 및 게시판 이외의 메뉴 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className="flex items-center px-10 py-6">
-      <a href="/">
-        <h1 className="font-bold text-2xl">🍪 Cookie Monster</h1>
-      </a>
+      <Link to="/">
+        <h1 className="font-bold text-2xl">tumuto</h1>
+      </Link>
       <ul className="flex gap-2 ml-10">
         <li>
-          <a href="/1" className="px-4 py-3 text-lg">게시판</a>
-        </li>
-        <li>
-          <a href="/1" className="px-4 py-3 text-lg">공지사항</a>
+          <Link to="/board" className="px-4 py-3 text-lg">게시판</Link>
         </li>
       </ul>
     </div>
