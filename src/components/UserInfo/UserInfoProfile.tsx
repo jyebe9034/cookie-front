18afb14138f85cc20a9babaed2ae4a962df81e59
davindev <davinjeong.dev@gmail.com>
2022-03-12 00:36:07 +0900

    fix: 유저 정보 영역 CSS 수정

--- src/components/UserInfo/UserInfoProfile.tsx
+++ src/components/UserInfo/UserInfoProfile.tsx
@@ -5,8 +5,8 @@ import Button from '@mui/material/Button';
 
 export default function UserInfoProfile() {
   return (
-    <div className="flex items-center p-4 border-b">
-      <div className="flex items-center">
+    <div className="flex items-center p-4">
+      <div className="flex flex-1 items-center">
         <Avatar sx={{ width: 50, height: 50 }} />
         <p className="ml-4 leading-tight line-clamp-2">
           아주아주 길어서 영역 밖으로 벗어나는 유저의 닉네임입니다. 말줄임표가 적용되어야 해요.
--- src/components/UserInfo/UserInfoWebtoon.tsx
+++ src/components/UserInfo/UserInfoWebtoon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UserInfoWebtoon() {
   return (
     <>
-      <p className="px-4 py-2 bg-gray-100 border-b">
+      <p className="px-4 py-2 bg-gray-100 border-y">
         내 취향 웹툰
       </p>
       <div className="overflow-y-auto h-96 px-4 py-4">
