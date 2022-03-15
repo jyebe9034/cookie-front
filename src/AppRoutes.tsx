eddd14878e9685594aa426e318c7e7e7ffee0c79
davindev <davinjeong.dev@gmail.com>
2022-03-16 00:04:21 +0900

    fix: Suspense, lazy 적용

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from 'components/Layout';
 
+const TestPage = lazy(() => import('pages/test/Test'));
+
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* <Route index element={...} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<p>Loading</p>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/test" element={<TestPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
