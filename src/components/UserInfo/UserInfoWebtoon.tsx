a6689e2093fbcbde751321acc46c41fec35fefa2
davindev <davinjeong.dev@gmail.com>
2022-03-23 23:08:25 +0900

    feat: 공통 Webtoon 컴포넌트 추가

--- src/components/UserInfo/UserInfoWebtoon.tsx
+++ src/components/UserInfo/UserInfoWebtoon.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import Webtoon from 'components/Webtoon';
+
 export default function UserInfoWebtoon() {
   return (
     <>
       <p className="px-4 py-2 bg-gray-100 border-y text-sm">
         내 취향 웹툰
       </p>
-      <div className="overflow-y-auto h-96 px-4 py-4">
-        {/* 웹툰 목록 노출 */}
+      <div className="overflow-y-auto h-80 divide-y">
+        <Webtoon />
+        <Webtoon />
+        <Webtoon />
+        <Webtoon />
+        <Webtoon />
       </div>
     </>
   );
