3c41789ecbf6e770c4fda722dabaf5ccc84b08e8
davindev <davinjeong.dev@gmail.com>
2022-04-16 18:16:31 +0900

    fix: 로그인 페이지에서 메인 페이지로 이동하는 링크 제거

--- src/pages/login/views/LoginView.tsx
+++ src/pages/login/views/LoginView.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import TomatoVideo from '../components/TomatoVideo';
 import NaverLoginButton from '../components/NaverLoginButton';
@@ -10,11 +9,9 @@ export default function Login() {
     <div className="flex items-center justify-center h-screen">
       <TomatoVideo />
       <div className="relative z-10 -mt-16">
-        <Link to="/">
-          <h1 className="font-display text-6xl text-center text-white">
-            tumuto
-          </h1>
-        </Link>
+        <h1 className="font-display text-6xl text-center text-white">
+          tumuto
+        </h1>
         <div className="grid gap-y-5 mt-28">
           <NaverLoginButton />
           <KakaoLoginButton />
