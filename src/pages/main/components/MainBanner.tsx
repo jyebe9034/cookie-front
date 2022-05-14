2652e8558114c1b4104dc2a871ebf044d01fcfeb
davindev <davinjeong.dev@gmail.com>
2022-05-14 19:55:52 +0900

    fix: 메인페이지 배너 수정

--- src/pages/main/components/MainBanner.tsx
+++ src/pages/main/components/MainBanner.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 
-import { ReactComponent as BannerImage } from 'assets/images/img_main_banner.svg';
+import BannerImage from 'assets/images/img_main_banner.jpeg';
 
 export default function MainBanner() {
   return (
-    <div className="h-[616px]">
-      <div className="flex justify-center overflow-hidden absolute top-0 left-0 -z-10 w-screen h-[700px] bg-red-50">
-        <div className="flex items-center relative w-[1300px]">
-          <h2 className="font-jalnan text-6xl leading-snug">
-            <span className="text-red-500">
-              투 머치 토커
-            </span>
-            들의
-            <br />
-            재미있는 세상 이야기
-          </h2>
-          <div className="absolute right-0 top-[150px] -z-10 scale-[2.5]">
-            <div className="-scale-x-100">
-              <BannerImage />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div
+      className="flex flex-col-reverse h-[35vw] p-10 bg-cover rounded-xl"
+      style={{
+        backgroundImage: `url(${BannerImage})`,
+        backgroundPosition: 'center 40%',
+      }}
+    >
+      <h2 className="font-jalnan text-[1.8vw] leading-snug text-white">
+        투 머치 토커들의
+        <br />
+        즐거운 웹툰 이야기
+      </h2>
     </div>
   );
 }
