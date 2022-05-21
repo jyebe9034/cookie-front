b9922b78690e087f3b4e908fa4f913ca820196c8
davindev <davinjeong.dev@gmail.com>
2022-05-21 18:03:58 +0900

    fix: PostItem 컴포넌트 분리

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,14 +1,11 @@
-/* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@mui/material/Button';
-import IconVisibility from '@mui/icons-material/Visibility';
-import IconFavorite from '@mui/icons-material/Favorite';
-import IconChat from '@mui/icons-material/Chat';
 
 import WebtoonThumbnail from 'components/WebtoonThumbnail';
-import Tag from 'components/Tag';
+
+import PostItemInfo from './PostItemInfo';
 
 export default function PostItem() {
   return (
@@ -18,35 +15,14 @@ export default function PostItem() {
         className="flex flex-1 items-center gap-x-5 mr-6"
       >
         <WebtoonThumbnail size="large" />
-        <div className="grid flex-1 gap-y-3">
-          <div className="flex gap-x-2">
-            <Tag name="네이버" />
-            <Tag name="드라마" />
-          </div>
-          <p className="text-lg truncate">
-            달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목 달글 제목
-          </p>
-          <div className="flex items-center gap-x-4 -mt-1">
-            <div className="flex items-center gap-x-1.5 text-gray-400">
-              <IconVisibility />
-              2,900
-            </div>
-            <div className="flex items-center gap-x-1.5 text-gray-400">
-              <IconFavorite />
-              2,900
-            </div>
-            <div className="flex items-center gap-x-1.5 text-gray-400">
-              <IconChat />
-              2,900
-            </div>
-          </div>
-        </div>
+        <PostItemInfo />
       </Link>
       <Button
         type="button"
         size="large"
         variant="outlined"
         disableElevation
+        sx={{ width: 150 }}
       >
         웹툰 보기
       </Button>
