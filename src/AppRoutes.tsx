e4d20b3e44a12dea61c30f620d33693552d118da
davindev <davinjeong.dev@gmail.com>
2022-06-04 17:06:54 +0900

    feat: 게시판 목록 페이지 추가

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -8,6 +8,7 @@ const TestPage = lazy(() => import('pages/test/views/TestView'));
 const JoinView = lazy(() => import('pages/join/views/JoinView'));
 const LoginPage = lazy(() => import('pages/login/views/LoginView'));
 const MainView = lazy(() => import('pages/main/views/MainView'));
+const BoardListView = lazy(() => import('pages/board/views/BoardListView'));
 const BoardEditView = lazy(() => import('pages/board/views/BoardEditView'));
 
 export default function AppRoutes() {
@@ -21,7 +22,9 @@ export default function AppRoutes() {
         <Route path="/" element={<Layout header footer />}>
           <Route path="/test" element={<TestPage />} />
           <Route path="/" element={<MainView />} />
+          <Route path="/board" element={<BoardListView />} />
           <Route path="/board/add" element={<BoardEditView />} />
+          <Route path="/board/:postId/edit" element={<BoardEditView />} />
         </Route>
       </Routes>
     </BrowserRouter>
